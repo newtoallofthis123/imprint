353f2354dba7ceddb3062f1869986865d2e774a4
newtoallofthis123 <noobscience123@gmail.com>
2024-06-04 23:54:41 +0530

    Add Options Constructor

--- node.h
+++ node.h
@@ -1,10 +1,14 @@
 #include "container.h"
+#include <map>
 
 namespace Approach {
 namespace Render {
 using std::string;
 class Node : public Container {
 public:
+  enum class Option : ProcUnit {
+    content,
+  };
   Node(const string content) {
     this->content = content;
     SetRenderID();
@@ -13,6 +17,24 @@ public:
     this->content = "";
     SetRenderID();
   };
+
+  Node(std::map<ProcUnit, void *> options) {
+    Node::SetRenderID();
+    SetOptions(options);
+  }
+
+  void SetOptions(std::map<ProcUnit, void *> options) {
+    std::map<ProcUnit, void *>::iterator option;
+    for (option = options.begin(); option != options.end(); ++option) {
+      switch ((Node::Option)option->first) {
+      case Option::content:
+        content = *(std::string *)option->second;
+        break;
+      default: /* generic option call; */
+        break;
+      }
+    }
+  }
 
   inline friend Node &operator<<(Node &to, Node &node) {
     to.nodes.push_back(&node);
